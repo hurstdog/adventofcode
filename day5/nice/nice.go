f4489c5dbad7db38e93cee74f8f7dc97381dc1c2
Andrew Hurst <andrew@hurstdog.org>
2016-01-10 10:30:35 -0800

    Base functions for testing naughty/nice strings

---
+++ day5/nice/nice.go
@@ -0,0 +1,49 @@
+// nice provides methods to determine if a given string is naughty or nice.
+package nice
+
+import (
+	"strings"
+)
+
+// Returns true if a string is nice, false, if it's naughty
+func nice(s string) bool {
+	return false
+}
+
+// repeats returns true if the string has a character that repeats itself.
+func repeats(s string) bool {
+	if len(s) < 2 {
+		return false
+	}
+	last := s[0]
+	for i := 1; i < len(s); i++ {
+		if last == s[i] {
+			return true
+		}
+		last = s[i]
+	}
+
+	return false
+}
+
+// vowels returns true if the string has at least three distinct vowels.
+func vowels(s string) bool {
+	var c int
+	for _, v := range []string{"a", "e", "i", "o", "u"} {
+		if strings.Index(s, v) >= 0 {
+			c++
+		}
+	}
+
+	return c >= 3
+}
+
+// banned returns true if the string contains banned character combinations
+func banned(s string) bool {
+	for _, v := range []string{"ab", "cd", "pq", "xy"} {
+		if strings.Index(s, v) >= 0 {
+			return true
+		}
+	}
+	return false
+}
---
+++ day5/nice/nice_test.go
@@ -0,0 +1,55 @@
+// Tests for the nice package
+package nice
+
+import (
+	"testing"
+)
+
+func TestHasRepeat(t *testing.T) {
+	testRepeat("abcde", false, t)
+	testRepeat("abcdd", true, t)
+	testRepeat("aabcd", true, t)
+	testRepeat("abababab", false, t)
+	testRepeat("", false, t)
+	testRepeat("a", false, t)
+}
+
+func testRepeat(s string, exp bool, t *testing.T) {
+	res := repeats(s)
+	if res != exp {
+		t.Errorf("Error: repeats(%s) == %v, expected %v", s, res, exp)
+	}
+}
+
+func TestVowels(t *testing.T) {
+	testVowels("", false, t)
+	testVowels("aei", true, t)
+	testVowels("bou", false, t)
+	testVowels("aeiou", true, t)
+	testVowels("abcdafgha", false, t)
+	testVowels("abicdafghoa", true, t)
+}
+
+func testVowels(s string, exp bool, t *testing.T) {
+	res := vowels(s)
+	if res != exp {
+		t.Errorf("Error: vowels(%s) == %v, expected %v", s, res, exp)
+	}
+}
+
+func TestBanned(t *testing.T) {
+	testBanned("", false, t)
+	testBanned("a", false, t)
+	testBanned("ab", true, t)
+	testBanned("cd", true, t)
+	testBanned("pq", true, t)
+	testBanned("xy", true, t)
+	testBanned("acb", false, t)
+}
+
+func testBanned(s string, exp bool, t *testing.T) {
+	res := banned(s)
+	if res != exp {
+		t.Errorf("Error: banned(%s) == %v, expected %v", s, res, exp)
+	}
+}
