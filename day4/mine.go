42713a6162a004fbbef37115df28c621a6fb63cb
Andrew Hurst <andrew@hurstdog.org>
2016-01-10 01:06:36 -0800

    Fixed, iterative version. With the correct answer.

--- day4/mine.go
+++ day4/mine.go
@@ -1,6 +1,7 @@
 // mine will run through all md5 hashes created by the string bgvyzdsv with
 // trailing numbers to find the lowest number that provides an md5 with 5
 // leading 0's.
+// Answer: 1038736. md5(bgvyzdsv1038736) = 000000b1b64bf5eb55aad89986126953
 package main
 
 import (
@@ -11,28 +12,23 @@ import (
 
 const INPUT = "bgvyzdsv"
 const MAXNUM = 1 << 62
-const CHECK = 3
 const PREFIX = "000000"
 
 func main() {
-	ch := make(chan string)
 	for i := int64(0); i < MAXNUM; i++ {
-		go testInt(i, ch)
-	}
-
-	// Print the first three results and quit
-	for i := 0; i < CHECK; i++ {
-		fmt.Println(<-ch)
+		r := testInt(i)
+		if r != "" {
+			fmt.Println(r)
+			break
+		}
 	}
 }
 
-func testInt(i int64, ch chan<- string) {
+func testInt(i int64) string {
 	t := fmt.Sprintf("%s%d", INPUT, i)
 	sum := fmt.Sprintf("%032x", md5.Sum([]byte(t)))
-	if i%10000 == 0 {
-		fmt.Printf("Test: %d: md5(%v) = %v\n", i, t, sum)
-	}
 	if strings.HasPrefix(sum, PREFIX) {
-		ch <- fmt.Sprintf("%d: md5(%v) = %v\n", i, t, sum)
+		return fmt.Sprintf("%d: md5(%v) = %v\n", i, t, sum)
 	}
+	return ""
 }
