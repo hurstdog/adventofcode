15a303beee91c60b32669b1020080496012fa0ed
Andrew Hurst <andrew@hurstdog.org>
2016-01-11 20:48:36 +1100

    Solution for part 1 (corrected to 5 0's) and part 2 (6 0's)

--- day4/mine.go
+++ day4/mine.go
@@ -1,7 +1,8 @@
 // mine will run through all md5 hashes created by the string bgvyzdsv with
 // trailing numbers to find the lowest number that provides an md5 with 5
 // leading 0's.
-// Answer: 1038736. md5(bgvyzdsv1038736) = 000000b1b64bf5eb55aad89986126953
+// 5 0's answer: 254575. md5(bgvyzdsv254575) = 000004b30d481662b9cb0c105f6549b2
+// 6 0's answer: 1038736. md5(bgvyzdsv1038736) = 000000b1b64bf5eb55aad89986126953
 package main
 
 import (
@@ -12,7 +13,7 @@ import (
 
 const INPUT = "bgvyzdsv"
 const MAXNUM = 1 << 62
-const PREFIX = "000000"
+const PREFIX = "00000"
 
 func main() {
 	for i := int64(0); i < MAXNUM; i++ {
