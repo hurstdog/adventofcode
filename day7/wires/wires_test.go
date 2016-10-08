ac754595cac5f59bbac607422695aa600cee07e3
Andrew Hurst <andrew@hurstdog.org>
2016-10-08 15:27:29 -0700

    Added tests of error handling.

--- day7/wires/wires_test.go
+++ day7/wires/wires_test.go
@@ -1,15 +1,31 @@
 // Tests for the Wires package
 package wires
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRunLine(t *testing.T) {
-	err := RunLine("123 -> x")
+	expectValue("123 -> x", "x", 123, t)
+	exp := fmt.Errorf("Line [123 -> x] assigns to a token that already exists.\n")
+	expectErr("123 -> x", exp, t)
+}
+
+func expectValue(s string, k string, v int, t *testing.T) {
+	err := RunLine(s)
 	if err != nil {
 		t.Error(err)
 	}
-	if C["x"] != 123 {
-		t.Errorf("Expecting x = 123, got %v\n", C["x"])
+	if C[k] != v {
+		t.Errorf("Expecting %v = %v, got %v\n", k, v, C[k])
+	}
+}
+
+func expectErr(s string, e error, t *testing.T) {
+	err := RunLine(s)
+	if err.Error() != e.Error() {
+		t.Errorf("Expected error [%v], got error [%v]\n", e, err)
 	}
 }
 
