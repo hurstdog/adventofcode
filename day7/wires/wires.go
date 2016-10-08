192d2bb6c14378557da07c51841e743ae8f55cd6
Andrew Hurst <andrew@hurstdog.org>
2016-10-08 16:25:53 -0700

    Support NOT. Refactor tests to be more flexible.

--- day7/wires/wires.go
+++ day7/wires/wires.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+const MASK16 = 0xFFFF
+
 // Types of connections:
 // const -> x
 // x [AND|OR|LSHIFT|RSHIFT] y -> z
@@ -19,15 +21,19 @@ import (
 // Circuit
 var C map[string]int = make(map[string]int)
 
+// A common signature for all line processing functions
+type linefunc func(string) error
+
 func RunLine(line string) error {
 	var err error
-	// Tokenize
-	tok := strings.Split(line, " ")
-	tokl := len(tok)
+	// num tokens is the number of spaces + 1
+	tokl := strings.Count(line, " ") + 1
 	if tokl < 3 {
 		return fmt.Errorf("Line [%v] has less than 3 tokens.\n", line)
 	} else if tokl == 3 {
-		return handleAssignment(tok)
+		return handleAssignment(line)
+	} else if tokl == 4 {
+		return handleNot(line)
 	} else {
 		return fmt.Errorf("Line [%v] has too many tokens.\n", line)
 	}
@@ -35,14 +41,44 @@ func RunLine(line string) error {
 	return err
 }
 
+// Processes the case of "NOT x -> y", returning an error on malformed input.
+func handleNot(line string) error {
+	tok := strings.Split(line, " ")
+	if len(tok) != 4 {
+		return fmt.Errorf("Too many tokens, expected 4, got %v, from [%v].\n",
+			len(tok), strings.Join(tok, " "))
+	}
+	if tok[0] != "NOT" {
+		return fmt.Errorf("Expected /^NOT.*/ with 4 tokens, got /%v/\n", line)
+	}
+	k := tok[1]
+	v, ok := C[k]
+	if !ok {
+		return fmt.Errorf("Token %v not yet defined, used in [%v]\n", k, line)
+	}
+	if tok[2] != "->" {
+		return fmt.Errorf("4 tokens implies '->' is the third, it isn't: [%v].\n",
+			strings.Join(tok, " "))
+	}
+	id := tok[3]
+	_, ok = C[id]
+	if ok {
+		return fmt.Errorf("Line [%v] assigns to a token that already exists.\n",
+			strings.Join(tok, " "))
+	}
+	C[id] = ^v & MASK16
+	return nil
+}
+
 // Processes the case of "N -> x", returning an error on malformed input.
-func handleAssignment(tok []string) error {
+func handleAssignment(line string) error {
+	tok := strings.Split(line, " ")
 	if len(tok) != 3 {
 		return fmt.Errorf("Too many tokens, expected 3, got %v, from [%v].\n",
 			len(tok), strings.Join(tok, " "))
 	}
 	if tok[1] != "->" {
-		return fmt.Errorf("3 tokens implies 2 == ->, it doesn't: [%v].\n",
+		return fmt.Errorf("3 tokens implies '->' is the second, it isn't: [%v].\n",
 			strings.Join(tok, " "))
 	}
 	val, err := strconv.Atoi(tok[0])
--- day7/wires/wires_test.go
+++ day7/wires/wires_test.go
@@ -6,14 +6,44 @@ import (
 	"testing"
 )
 
-func TestRunLine(t *testing.T) {
-	expectValue("123 -> x", "x", 123, t)
-	exp := fmt.Errorf("Line [123 -> x] assigns to a token that already exists.\n")
-	expectErr("123 -> x", exp, t)
+func resetC() {
+	for k, _ := range C {
+		delete(C, k)
+	}
 }
 
-func expectValue(s string, k string, v int, t *testing.T) {
-	err := RunLine(s)
+func TestResetC(t *testing.T) {
+	expectValue(RunLine, "123 -> x", "x", 123, t)
+	resetC()
+	expectValue(RunLine, "123 -> x", "x", 123, t)
+}
+
+func TestHandleAssignment(t *testing.T) {
+	resetC()
+	expectValue(handleAssignment, "123 -> x", "x", 123, t)
+	exp := fmt.Errorf("Line [123 -> x] assigns to a token that already exists.\n")
+	expectErr(handleAssignment, "123 -> x", exp, t)
+}
+
+func TestHandleNot(t *testing.T) {
+	resetC()
+
+	exp := fmt.Errorf("Expected /^NOT.*/ with 4 tokens, got /ASD x -> h/\n")
+	expectErr(handleNot, "ASD x -> h", exp, t)
+
+	exp = fmt.Errorf("Token x not yet defined, used in [NOT x -> h]\n")
+	expectErr(handleNot, "NOT x -> h", exp, t)
+
+	handleAssignment("123 -> x")
+	handleAssignment("123 -> y")
+	exp = fmt.Errorf("Line [NOT x -> y] assigns to a token that already exists.\n")
+	expectErr(handleNot, "NOT x -> y", exp, t)
+
+	expectValue(handleNot, "NOT x -> h", "h", 65412, t)
+}
+
+func expectValue(fn linefunc, s string, k string, v int, t *testing.T) {
+	err := fn(s)
 	if err != nil {
 		t.Error(err)
 	}
@@ -22,8 +52,8 @@ func expectValue(s string, k string, v int, t *testing.T) {
 	}
 }
 
-func expectErr(s string, e error, t *testing.T) {
-	err := RunLine(s)
+func expectErr(fn linefunc, s string, e error, t *testing.T) {
+	err := fn(s)
 	if err.Error() != e.Error() {
 		t.Errorf("Expected error [%v], got error [%v]\n", e, err)
 	}
