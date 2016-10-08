ed0e262c9df8e61d17c26f341230296f0a907433
Andrew Hurst <andrew@hurstdog.org>
2016-10-08 15:58:13 -0700

    Removed old test code, factored out handling assignments.

--- day7/wires/wires.go
+++ day7/wires/wires.go
@@ -27,23 +27,34 @@ func RunLine(line string) error {
 	if tokl < 3 {
 		return fmt.Errorf("Line [%v] has less than 3 tokens.\n", line)
 	} else if tokl == 3 {
-		if tok[1] != "->" {
-			return fmt.Errorf("3 tokens implies 2 == ->, it doesn't: [%v].\n", line)
-		}
-		// const -> x
-		val, err := strconv.Atoi(tok[0])
-		if err != nil {
-			return err
-		}
-		id := tok[2]
-		_, ok := C[id]
-		if ok {
-			return fmt.Errorf("Line [%v] assigns to a token that already exists.\n", line)
-		}
-		C[id] = val
+		return handleAssignment(tok)
 	} else {
 		return fmt.Errorf("Line [%v] has too many tokens.\n", line)
 	}
 
 	return err
+}
+
+// Processes the case of "N -> x", returning an error on malformed input.
+func handleAssignment(tok []string) error {
+	if len(tok) != 3 {
+		return fmt.Errorf("Too many tokens, expected 3, got %v, from [%v].\n",
+			len(tok), strings.Join(tok, " "))
+	}
+	if tok[1] != "->" {
+		return fmt.Errorf("3 tokens implies 2 == ->, it doesn't: [%v].\n",
+			strings.Join(tok, " "))
+	}
+	val, err := strconv.Atoi(tok[0])
+	if err != nil {
+		return err
+	}
+	id := tok[2]
+	_, ok := C[id]
+	if ok {
+		return fmt.Errorf("Line [%v] assigns to a token that already exists.\n",
+			strings.Join(tok, " "))
+	}
+	C[id] = val
+	return nil
 }
