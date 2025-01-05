1ec22d43c93e2f2dd71097cb32bb51ef3308c9bb
Andrew Hurst <andrew@hurstdog.org>
2025-01-05 09:47:43 -0800

    Solved part 2 of day 1

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -6,6 +6,7 @@ limiter = 100000
 
 left = []
 right = []
+right_count = {}
 
 for line in f:
     # strip the newline
@@ -14,13 +15,22 @@ for line in f:
     parts = line_s.split()
     left.append(parts[0])
     right.append(parts[1])
+
+    # add counts of the number of times we've seen numbers on the right
+    if right_count.get(parts[1]) is None:
+        right_count[parts[1]] = 1
+    else:
+        right_count[parts[1]] = right_count[parts[1]] + 1
+
     limiter = limiter - 1
     if limiter <= 0:
         break
 
 #print("Left: %s" % left)
 #print("Right: %s" % right)
+#print("Right count: %s" % right_count)
 
+### part 1 solution section
 # sort each list
 left.sort()
 right.sort()
@@ -33,3 +43,16 @@ for i in range(len(left)):
     #print("Difference: %s, total: %s" % (diff, total_diff))
 
 print("Total difference: %s" % total_diff)
+
+### part 2 solution section
+# Iterate over the left list, multiplying by the count on the right list
+similarity = 0
+for i in range(len(left)):
+    num = left[i]
+    multiplier = 0
+    if right_count.get(num) is not None:
+        multiplier = right_count[num]
+    #print("num: %s; multiplier: %s" % (num, multiplier))
+    similarity = similarity + (int(num) * multiplier)
+
+print("Part 2 Similarity: %s" % similarity)
