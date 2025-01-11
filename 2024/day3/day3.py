d12069a817453caa2341378f5514f972359f0245
Andrew Hurst <andrew@hurstdog.org>
2025-01-11 10:37:48 -0800

    Support catching the first couple mul statements in the test input

--- 2024/day3/day3.py
+++ 2024/day3/day3.py
@@ -6,11 +6,42 @@ INPUT = "2024/day3/input.txt"
 Class for storing the input text, and returning the next command on each request.
 """
 class Day3Reader:
+    """
+    all_text: str - contains the full input text from a day3 file.
+    """
     def __init__(self, all_text):
         self.all_text = all_text
+        self.i = 0      # pointer into the string
+        self.text_len = len(all_text)
 
     def returnNextCommand(self):
-        return "mul(3,2)"
+        # start from last iteration point, and keep reading until we find
+        # the start of a command. Continue, skipping characters that are 
+        # invalid, unless we see the start of another command ("mul(")
+        cmd = ""
+        # skip to the next mul( start.
+        while ((self.i + 4) < self.text_len and 
+               self.all_text[self.i:self.i+4] != "mul("):
+            self.i += 1
+
+        # read until closing paren, but don't loop forever
+        # this should say command length
+        cmd_len = 3
+        while ((self.i + cmd_len) < self.text_len and
+               self.all_text[self.i+cmd_len] != ")"):
+            cmd_len += 1
+            print(f"Current Command: '{self.all_text[self.i:self.i+cmd_len+1]}'")
+        
+        print(f"i: {self.i}, cmd_len: {cmd_len}")
+
+        # build the command
+        cmd = self.all_text[self.i:self.i+cmd_len+1]
+        print(f"Current Command: '{cmd}'")
+
+        # update the next position to start
+        self.i = self.i + cmd_len + 1
+        
+        return cmd
     
     def totalSum(self):
         return 0
@@ -24,7 +55,7 @@ def testDay3():
     tests = ["mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)"]
     for t in tests:
         cmd = dr.returnNextCommand()
-        assert cmd == t, f"Assertion failed: cmd is {cmd}, but expected {t}"
+        assert cmd == t, f"FAIL: cmd is '{cmd}', but expected '{t}'"
 
 def executeDay3():
     f = open(INPUT, "r")
