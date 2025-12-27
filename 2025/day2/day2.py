51c9dff1a40da765ee4732cd08f1d0fbb8172323
Andrew Hurst <andrew@hurstdog.org>
2025-12-27 11:26:53 -0800

    Added code to sum op invalid ids in a range

--- 2025/day2/day2.py
+++ 2025/day2/day2.py
@@ -15,14 +15,11 @@ def isValidID(id):
     midpoint = int(l / 2)
     p1 = idstr[:midpoint]
     p2 = idstr[midpoint:]
-    if DEBUG:
+    if DEBUG and 0:
         print(f"Comparing {p1} to {p2}")
     if (p1 == p2):
         return False
     return True
-
-def countInvalidIDsInRange(range):
-    return 0
 
 def testIsValidID():
     cases = [
@@ -48,11 +45,62 @@ def testIsValidID():
                 
     return success
 
-def testCountInvalidIDsInRange():
-    return 0
+def rangeToSequence(r):
+    # split on the -
+    (start, end) = r.split("-")
+
+    # int the first
+    istart = int(start)
+    iend = int(end)
+
+    ret = []
+    i = int(start)
+    while i <= iend:
+        ret.append(i)
+        i = i + 1
+
+    # return the array
+    return ret
+
+def sumInvalidIDsInRange(r):
+    seq = rangeToSequence(r)
+    tot = 0
+    for num in seq:
+        if not isValidID(num):
+            tot = tot + num
+
+    return tot
+
+def testSumInvalidIDsInRange():
+    ranges = [
+        ("11-22", 33),
+        ("95-115", 99),
+        ("998-1012", 1010),
+        ("1188511880-1188511890", 1188511885),
+        ("222220-222224", 222222),
+        ("1698522-1698528", 0),
+        ("446443-446449", 446446),
+        ("38593856-38593862", 38593859),
+        ("565653-565659", 0),
+        ("824824821-824824827", 0),
+        ("2121212118-2121212124", 0)
+    ]
+
+    success = True
+    for (r, s) in ranges:
+        res = sumInvalidIDsInRange(r)
+        if res == s:
+            if DEBUG:
+                print(f"SUCCESS: Sum of invalid ids in {r} is {s}")
+        else:
+            success = False
+            if DEBUG:
+                print(f"FAIL: Sum of invalid ids in {r} is {res}, expected {s}")
+
+    return success
 
 def main():
-    if not testIsValidID():
+    if not testIsValidID() or not testSumInvalidIDsInRange():
         print("**** STOPPING EXECUTION AFTER FAILURES ****")
         sys.exit(1)
 
